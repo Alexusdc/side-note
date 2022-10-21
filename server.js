8f35b2dbbde5048157836bba0058bddc4c9a1374
alexuschristensen <alexusdc6@gmail.com>
2022-10-20 19:56:13 -0700

    add Heroku

--- server.js
+++ server.js
@@ -8,20 +8,24 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
+// Connecting the html page
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
+// Connecting the note page
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+// Connecting the json database
 app.get('/api/notes', (req, res) => {
     const db = JSON.parse(fs.readFileSync('./db/db.json'));
 
     res.json(db)
 });
 
+// Creating the ability to write and save notes 
 app.post('/api/notes', (req, res) => {
     const db = JSON.parse(fs.readFileSync('./db/db.json'));
 
